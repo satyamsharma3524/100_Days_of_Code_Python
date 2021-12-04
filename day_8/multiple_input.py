a7de36e33acffe8a261396cc878137acde24097f
satyamsharma3524 <satyamsharma3524@gmail.com>
2021-12-04 12:23:32 +0530

    Difference between Positional arguments and keyword arguments.

--- day_8/multiple_input.py
+++ day_8/multiple_input.py
@@ -3,8 +3,11 @@ def greet_with(name,location):
     print(f"How's the weather in {location}.")
 
 
-name = input("Enter your name : \n")
-location = input("What is your location : \n")
+names = input("Enter your name : \n")
+locations = input("What is your location : \n")
 
 # Here name and location is positional arguments because if we switch their position the data that is passing to the fucntion also gets switched.
-greet_with(name,location)
+greet_with(names,locations)
+
+# Here name and location is keyword arguments because we have assigned a keyword to them during function call.
+greet_with(name = names,location = locations)
