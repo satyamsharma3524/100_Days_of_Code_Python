a7a8af5d61092d08192ad4efa3f2b5125ac6ae0e
satyamsharma3524 <satyamsharma3524@gmail.com>
2021-12-04 12:04:55 +0530

    simple functions and functions with input

--- day_8/functions_input.py
+++ day_8/functions_input.py
@@ -1,4 +1,13 @@
+# function that allows for the input 
 def greet(name):
     print(f"hello! {name}")
 
+# here (name) is the parameter and "satyam" is the argument.
 greet("Satyam")
+
+# simple function 
+def greeting():
+    print("Hello buddy!")
+    print("What are you doing?")
+
+greeting()
