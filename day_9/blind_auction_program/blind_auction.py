3e6d56149020bb99a86f1d52e6af2e5bbf8ebe28
satyamsharma3524 <satyamsharma3524@gmail.com>
2021-12-07 20:32:01 +0530

    Completing blind auction program

--- day_9/blind_auction_program/blind_auction.py
+++ day_9/blind_auction_program/blind_auction.py
@@ -1,3 +1,26 @@
 # click library is here to use the click.clear() fucntion to clear the terminal screen.
 import click
+import art
+print(art.logo)
+stop_auction = False
+players = {}
 
+def auction(name,bid_amount):
+    players[name] = bid_amount
+    winner = max(players,key=players.get)
+
+    return winner
+
+while not stop_auction:
+    name = input("What is your name? \n")
+    bid_amount = int(input("Enter your Bid amount : \n"))
+    next_player = input("If there are other users who want to bid then type 'yes' or type 'no'.").lower()
+
+    winner = auction(name=name,bid_amount=bid_amount)
+
+    if next_player == "no":
+        stop_auction = True
+        click.clear()
+        print(f"The Auction winner is : {winner}")
+    elif next_player == "yes":
+        click.clear()
