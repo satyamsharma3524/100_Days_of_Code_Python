f0d6fbfa223da58af993cdba55be96fa5fdbf243
satyamsharma3524 <satyamsharma3524@gmail.com>
2021-12-08 19:09:48 +0530

    creating documentation of our user-defined (self-defined) fucntions.

---
+++ day_10/doc_strings.py
@@ -0,0 +1,22 @@
+def format_name(f_name,l_name,format):
+    # here this line below represents the documentation of the current fucntion. 
+    """This function takes first_name and last_name and return the formatted output of selected format type."""
+    if format=="lower":
+        f_name = f_name.lower()
+        l_name = l_name.lower()
+    elif format=="upper":
+        f_name = f_name.upper()
+        l_name = l_name.upper()
+    elif format=="title":
+        f_name = f_name.title()
+        l_name = l_name.title()
+    name = f_name + " " + l_name
+    return name
+
+f_name = input("Enter first name : \n")
+l_name = input("Enter last name : \n")
+format = input("Enter formatting method : \n 'lower' for lower case \n 'upper' for upper case \n 'title' for title case  \n").lower()
+formated_name = format_name(f_name=f_name,l_name=l_name,format = format)
+
+
+print(f"Your {format} case name is {formated_name}")
